76753eac5662ecd263bfa5d510b7af58d2c473b9
Anjaiah Martha <anjaiahmartha59@gmail.com>
2024-03-26 11:46:17 +0530

    add two num

---
+++ src/test/java/GitDemo/Addtwonumbers.java
@@ -0,0 +1,12 @@
+package GitDemo;
+
+public class Addtwonumbers {
+public static int add_num(int x,int y) {
+	int z=x+y;
+	return z;
+	//System.out.println(z);
+}
+public static void main(String[] args) {
+	System.out.println(add_num(3,6));
+}
+}
