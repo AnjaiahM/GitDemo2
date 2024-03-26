90b010dcce6a9728c98dd8e6a38493fd50ea45a9
Anjaiah Martha <anjaiahmartha59@gmail.com>
2024-03-26 12:40:31 +0530

    substract

--- src/test/java/GitDemo/Addtwonumbers.java
+++ src/test/java/GitDemo/Addtwonumbers.java
@@ -6,7 +6,13 @@ public static int add_num(int x,int y,int z) {
 	return a;
 	//System.out.println(z);
 }
+public static int sub_num(int x,int y) {
+	int a=x-y;
+	return a;
+	//System.out.println(z);
+}
 public static void main(String[] args) {
 	System.out.println(add_num(3,3,3));
+	System.out.println(sub_num(5,2));
 }
 }
