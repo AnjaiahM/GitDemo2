e9622b675448ee63d1a6a334c5c7d2267ac943d1
Anjaiah Martha <anjaiahmartha59@gmail.com>
2024-03-26 13:11:22 +0530

    multi commit

--- src/test/java/GitDemo/Addtwonumbers.java
+++ src/test/java/GitDemo/Addtwonumbers.java
@@ -11,8 +11,14 @@ public static int sub_num(int x,int y) {
 	return a;
 	//System.out.println(z);
 }
+public static int mul_num(int x,int y,int z) {
+	int a=x*y*z;
+	return a;
+	//System.out.println(z);
+}
 public static void main(String[] args) {
 	System.out.println(add_num(3,3,3));
 	System.out.println(sub_num(5,2));
+	System.out.println(mul_num(5,2,6));
 }
 }
