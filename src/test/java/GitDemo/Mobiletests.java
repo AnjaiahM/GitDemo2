b86248b5194bf27f84a5a11bc116fc64b36936be
Anjaiah Martha <anjaiahmartha59@gmail.com>
2024-03-26 18:56:53 +0530

    develop branch

---
+++ src/test/java/GitDemo/Mobiletests.java
@@ -0,0 +1,10 @@
+package GitDemo;
+
+public class Mobiletests {
+public static void main(String[] args) {
+	System.out.println("iomobile");
+	System.out.println("androidmobile");
+	System.out.println("redmemobile");
+	System.out.println("sumsungmobile");
+}
+}
